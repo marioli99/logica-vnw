3a6435728341c58b65105f9b0181c2ccbe80df63
marioli99 <mazmariana.mo@gmail.com>
2022-01-27 14:45:17 -0300

    Add atividade 5

---
+++ 05 - Estrutura de dados simples/script.js
@@ -0,0 +1,21 @@
+// Crie um array que receba 5 itens e exiba no console.
+let mercado = ["Arroz", "Feijão","Macarrão", "Café", "Açucar" ]
+console.log(mercado)
+
+// Utilize um método para adicionar um nome ao inicio do array.
+mercado.unshift('Manga');
+// Utilize um método para remover o último nome do array.
+mercado.pop()
+
+// Utilize um método para adicionar dois nomes ao fim do array.
+
+mercado.push('Banana', 'Nescau')
+
+// Utilize um método para remover o primeiro nome do array.
+ mercado.shift()
+
+// Utilize um método para organizar em ordem crescente o seguinte array:
+// const numbers = [7,5,6,3,8,9,2,1,4]
+const numbers = [7,5,6,3,8,9,2,1,4]
+numbers.sort()
+console.log(numbers)
